0004cedf7ffa5c39ddf3cb86f09847c7bfee3771
paw <pawsen@gmail.com>
2019-06-27 20:04:25 -0500

    PNLSS benchmark3 Nm > 1. First try

--- benchmark3/ode45_multisine.m
+++ benchmark3/ode45_multisine.m
@@ -10,9 +10,11 @@ clear all
 srcdir = '../src/matlab';
 addpath(genpath(srcdir));
 
-dataname = 'ms_odd_random';
+dataname = 'ms_full';
 savedata = true;
 savefig = true;
+
+benchmark = 3;
 
 %% Define system
 
@@ -25,14 +27,14 @@ thickness = .001;
 R=3;
 
 [L,rho,E,~,PHI,~,gam] = beams_for_everyone(setup,Nmod*2-1,thickness);
-PHI_L2 = PHI(L/2);
-PHI_L2 = PHI_L2(1:2:end);
+PHIS = PHI([L/4; L/2; 3*L/4]);
+PHIS = PHIS(:,1:2:end);
 gam = gam(1:2:end);
 
 % load nonlinear coefficients (can be found e.g. via IC-method)
 modelname  = ['data/','beam_msh_80_4_1_3t_steel_',...
     num2str(thickness*1000),'mm_R',num2str(R),'m.mat'];
-[p, E] = nlcoeff(modelname, Nmod);
+[p, E] = nlcoeff(modelname, Nmod, benchmark);
 
 % also om is needed for linear model, so we load the model again
 load(modelname);
@@ -43,52 +45,53 @@ M = eye(Nmod);
 D = diag(2*Dmod(:).*om(:));
 K = diag(om.^2);
 
-
 % Fundamental harmonic of external forcing
 Fex1 = gam;
 
-% Define oscillator as system with polynomial stiffness nonlinearities
-oscillator = System_with_PolynomialStiffnessNonlinearity(M,D,K,p,E,Fex1);
-
 % Number of degrees of freedom
-n = oscillator.n;
+n = Nmod;
 
 %% multisine, using time domain formulation
 
-R = 1;           % Realizations. (one for validation and one for testing)
-P = 5;           % Periods, we need to ensure steady state
+R  = 4;           % Realizations. (one for validation and one for testing)
+P  = 8;           % Periods, we need to ensure steady state
 f1 = 5;          % low freq
-f2 = 450;        % high freq
-fs = 1500;       % 5*f2. Must be fs>2*f2. Nyquist freq, you know:)
-f0 = 1;          % freq resolution. 
-N = f2/f0;       % freq points
-A = 15;          % amplitude
-Nt = fs/f0;      % time points per period
+f2 = 405;        % high freq
+N  = 1e3;         % freq points
+f0 = (f2-f1)/N;
+A  = 15;          % amplitude
 % set the type of multisine
-ms_type = 'random_odd';  % can be 'full', 'odd' or 'random_odd'
-ms = multisine_lines(N,Nt, ms_type);
+ms_type = 'full';  % can be 'full', 'odd' or 'random_odd'
 
-q0 = zeros(n,1);
-u0 = zeros(n,1);
-t1 = 0;
-t2 = P/f0;
+Nt = 2^13;      % Time per cycle
+fs = Nt*f0;     % Samping frequency
 
-t = t1:1/fs:t2-1/fs;   % time vector. ode45 interpolates output
-freq = (0:Nt-1)/Nt*fs; % frequency content
-nt = length(t);        % total number of points
+if fs/2 <= f2
+    error('Increase sampling rate!');
+end
 
+q0   = zeros(n,1);
+u0   = zeros(n,1);
+t1   = 0;
+t2   = P/f0;
+% time vector. ode45 interpolates output
+t    = linspace(t1, t2, Nt*P+1);  t(end) = [];
+% t1 = t1:1/fs:t2-1/fs;
+freq = (0:Nt-1)/Nt*fs;   % frequency content
+nt = length(t);          % total number of points
+
+Avec = [1,15,50];
+for A=Avec
 u = zeros(Nt,P,R);
 y = zeros(Nt,P,R,n);
 ydot = zeros(Nt,P,R,n);
 phases = zeros(N,R);
-
+MS = cell(R, 1);
+    
 tic
 for r=1:R
-    % predictable pseudo-random numbers
-    rng(r);
-    % multisine in time domain (sum of sines)
-    phase = 2*pi*rand(N,1);
-    fex = @(t) ms.har'*A*cos(2*pi*(1:N)'*f0*t + phase) / sqrt(sum(ms.har));
+    % multisine force signal
+    [fex, MS{r}] = multisine(f1, f2, N, A, [], [], r);
 
     par = struct('M',M,'C',D,'K',K,'p',p,'E',E,'fex',fex, 'amp', Fex1);
     [tout,Y] = ode45(@(t,y) sys(t,y, par), t,[q0;u0]);
@@ -96,22 +99,24 @@ for r=1:R
     u(:,:,r) = reshape(fex(tout'), [Nt,P]);
     y(:,:,r,:) = reshape(Y(:,1:n), [Nt,P,n]);
     ydot(:,:,r,:) = reshape(Y(:,n+1:end), [Nt,P,n]);
-    phases(:,r) = phase;
 end
 disp(['ode45 with multisine in time domain required ' num2str(toc) ' s.']);
 
 if savedata
-save(['data/',dataname],'u','y','ydot','f1','f2','fs','f0','freq',...
-    't','A','PHI_L2','ms','phases')
+    save(sprintf('data/A%d_%s',A,dataname),'u','y','ydot','f1','f2',...
+        'fs','freq','t','A','PHIS', 'MS')
+end
 end
 
 %% init plotting
 hfig = {};
 % select realization to do the plotting for
 r = 1;
+% select measurement point, ie. the mode factor in PHIS(np,:)
+np = 1;
 
 %% show time series
-yres = PHI_L2*reshape(y(:,:,r,:),[],n)';
+yres = PHIS(np,:)*reshape(y(:,:,r,:),[],n)';
 
 figure;
 plot(tout, yres,'k-')
@@ -166,26 +171,26 @@ hfig(end+1) = {{gcf, "ms_exfreq"}};
 
 %% show detect type of nonlinearity
 
-% output signal for one period
-% plot only half spectrum(single sided spectrum)
-dft = fft(y(:,end,r));
-nt = length(dft)/2+1;
-xdft = dft(1:nt);
-Ydot = fft(ydot(:,end,r));
-
-figure;
-hold on
-plot(freq(ms.lines),db(xdft(ms.lines)),'.k')
-scatter(freq(ms.non_odd),db(xdft(ms.non_odd)),[])
-scatter(freq(ms.non_even),db(xdft(ms.non_even)),[])
-hold off
-
-xlabel('frequency (Hz)')
-ylabel('magnitude (dB)')
-title('FFT of one period of the output spectrum')
-legend('excited','non\_odd','non\_even')
-
-hfig(end+1) = {{gcf, "ms_nl_detection"}};
+% % output signal for one period
+% % plot only half spectrum(single sided spectrum)
+% dft = fft(y(:,end,r));
+% nt = length(dft)/2+1;
+% xdft = dft(1:nt);
+% Ydot = fft(ydot(:,end,r));
+% 
+% figure;
+% hold on
+% plot(freq(ms.lines),db(xdft(ms.lines)),'.k')
+% scatter(freq(ms.non_odd),db(xdft(ms.non_odd)),[])
+% scatter(freq(ms.non_even),db(xdft(ms.non_even)),[])
+% hold off
+% 
+% xlabel('frequency (Hz)')
+% ylabel('magnitude (dB)')
+% title('FFT of one period of the output spectrum')
+% legend('excited','non\_odd','non\_even')
+% 
+% hfig(end+1) = {{gcf, "ms_nl_detection"}};
 
 %% save figures
 if savefig
---
+++ benchmark3/run_benchmark.m
@@ -0,0 +1,220 @@
+clear all
+clc
+
+benchmark = 1;
+Nt = 1024;
+
+setup = 'New_Design_Steel';
+switch benchmark
+    case 1
+        Dmod = [.38]*.01;
+        Nmod = 1;
+        imods = [1];
+        Hs = [1:2:40 40];  % only unenven harmonics
+        exc_lev = [10,40,60,80,100];
+        % start and end vibration level (log10 of modal mass)
+        log10a_ss = [1]*(-5);
+        log10a_ee = [1]*(-3.2);
+        Nhs = 3;
+    case 2
+        Dmod = [.38 .12 .09 .08 .08]*.01;
+        Nmod = 5;
+        thickness = .001;
+        % load nonlinear coefficients (can be found e.g. analytically)
+        modelname = ['data/beam_New_Design_Steel_analytical_5t_' ...
+            num2str(thickness*1000) 'mm.mat'];
+
+        imods = [1,2,3];
+        Hs = [1:2:40 40];  % only unenven harmonics
+        exc_lev = [10,40,60,80,100];
+        % start and end vibration level (log10 of modal mass)
+        log10a_ss = [1,1,1]*(-5);
+        log10a_ee = [1,1,1]*(-3.2);
+        Nhs = ones(lsize(imods))*7;
+    case 3
+        % Fundamental parameters
+        Dmod = [.38 .09 .08]*.01; % first, third, fifth bending modes of flat beam
+        Nmod = 3;
+        thickness = .001;
+        R=3;
+        % load nonlinear coefficients (can be found e.g. via IC-method)
+        modelname  = ['data/beam_msh_80_4_1_3t_steel_',...
+            num2str(thickness*1000),'mm_R',num2str(R),'m.mat'];
+        
+        imods = [1,2,3];
+        % Convergence study for number of harmonics
+        Hs = [1:1:40];
+        % Excitation levels
+        exc_lev = [10,30,50];
+        log10a_ss = [1,1,1]*(-8);
+        log10a_ee = [1,1,1]*(-3.2);
+        Nhs = ones(lsize(imods))*7;
+end
+
+%% load analytical parameters
+[L,rho,E,om,PHI,~,gam] = beams_for_everyone(setup,Nmod*2-1,thickness);
+PHI_L2 = PHI(L/2);
+PHI_L2 = PHI_L2(1:2:end);
+
+% load nl coefficient
+[p, E] = nlcoeff(modelname, Nmod, benchmark);
+
+if benchmark == 3
+    gam = gam(1:2:end);
+    % om is needed for linear model, so we load the model directly
+    load(modelname);
+    om = model.omega;
+end
+
+% Properties of the underlying linear system
+M = eye(Nmod);
+D = diag(2*Dmod(:).*om(:));
+K = diag(om.^2);
+
+% Fundamental harmonic of external forcing
+Fex1 = gam;
+
+% Define oscillator as system with polynomial stiffness nonlinearities
+oscillator = System_with_PolynomialStiffnessNonlinearity(M,D,K,p,E,Fex1);
+% Number of degrees of freedom
+ndof = oscillator.n;
+
+% Linear modal analysis
+[PHI_lin,OM2] = eig(oscillator.K,oscillator.M);
+[om_lin,ind] = sort(sqrt(diag(OM2)));
+PHI_lin = PHI_lin(:,ind);
+
+
+%% hb settings
+% start and end frequency in rad/s
+switch benchmark
+    case 1
+        Om_ss = [om(1)*.2];
+        Om_ee = [3*om(1)];
+    case 2
+        Om_ss = [om(1)*.2, om(2)*.75, om(3)*.75];
+        Om_ee = [3*om(1), om(2)*1.20, om(3)*1.20];
+    case 3
+        Om_ss = [0, 1300*2*pi, 3400*2*pi];
+        Om_ee = [3*om_lin(1), 1600*2*pi, 3700*2*pi];
+end
+
+for imod = imods
+
+Om_s = Om_ss(imod);
+Om_e = Om_ee(imod);
+
+%% convergence analysis
+% Set excitation level
+oscillator.Fex1 = Fex1*exc_lev(end);
+Nt = 1024;
+Xc = cell(size(Hs));
+PkPs = zeros(length(Hs), 2);
+for h = 1:length(Hs)
+    Nh = Hs(h);
+    [X,Solinfo,Sol] = hb_frf(Om_s, Om_e, Nh, Nt,oscillator);
+    
+    % calculate 90 degree phase for current mode
+    % Sc = [freq, amp, phase]
+    Sc = [X(end, :);
+        sqrt([1 0.5*ones(1,2*Nh)]*X(imod:ndof:end-1,:).^2);
+        atan2d(-X(2*ndof+imod,:), X(ndof+imod,:))]';
+    PkPs(h,:) = interp1(Sc(:,3), Sc(:,1:2), -90, 'pchip');
+    Xc{h} = X;
+    fprintf('%d/%d Done.\n', h, length(Hs)) 
+end
+
+% Convergence Criterion
+errmax = 0.01*1e-2;
+Errs = abs(PkPs-PkPs(end,:))./PkPs;
+ihconv = find(max(Errs,[],2)<=errmax, 1 );
+Nhconv = Hs(ihconv);
+
+save(sprintf('data/b%d_HConvDat_M%d.mat',benchmark,imod),...,
+    'Xc', 'PkPs', 'Errs', 'errmax', 'Nhconv', 'ihconv','Hs','exc_lev')
+
+%% Compute frequency response using harmonic balance
+Nh = Nhs(imod);
+Nt=2*3*Nh+1;
+
+X = cell(size(exc_lev));
+for iex=1:length(exc_lev)
+    % Set excitation level
+    oscillator.Fex1 = Fex1*exc_lev(iex);
+    [X{iex},Solinfo,Sol] = hb_frf(Om_s, Om_e, Nh, Nt,oscillator);
+    
+end
+% create struct from varible names. Short hacky way.
+frfdata = ws2struct('X','ds','Nh','Nt','exc_lev','oscillator','ndof'...
+    ,'Dmod','Nmod','om','gam','thickness','setup','modelname','R','PHI_L2');
+if savedata
+    save(sprintf('data/b%d_frf_M%d.mat',benchmark,imod),'-struct','frfdata')
+end
+
+%% NMA
+
+Nh = Nhs(imod);
+Nt=2*3*Nh+1;
+log10a_s = log10a_ss(imod)-8;
+log10a_e = log10a_ee(imod)-3.2;
+
+[X,Solinfo,Sol] = hb_nma(log10a_s,log10a_e, Nh, Nt,oscillator,...
+    om_lin, PHI_lin);
+
+nmadata = ws2struct('Solinfo','Sol','X','ds','Nh','Nt','ndof','imod',...
+    'oscillator','Dmod','Nmod','om','gam','thickness','setup',...
+    'modelname','R','PHI_L2','Sopt');
+if savedata
+    save(sprintf('data/nma_M%d.mat',imod),'-struct','nmadata')
+end
+
+
+end
+
+
+function [X,Solinfo,Sol] = hb_frf(Om_s, Om_e, Nh, Nt,oscillator)
+
+K = oscillator.K;
+M = oscillator.M;
+D = oscillator.D;
+
+% Initial guess (solution of underlying linear system)
+Q1 = (-Om_s^2*M + 1i*Om_s*D + K)\oscillator.Fex1;
+y0 = zeros((2*Nh+1)*length(Q1),1);
+y0(length(Q1)+(1:2*length(Q1))) = [real(Q1);-imag(Q1)];
+
+ds = 50; % -> better for exc_lev = 50
+Dscale = [1e-6*ones(length(y0),1);(Om_s+Om_e)/2];
+Sopt = struct('Dscale',Dscale,'dynamicDscale',1,'jac','full',...
+    'stepmax',1e4);
+
+[X,Solinfo,Sol] = solve_and_continue(y0,...
+    @(X) HB_residual(X,oscillator,Nh,Nt,'frf'),...
+    Om_s,Om_e,ds,Sopt);
+
+end
+
+function [X,Solinfo,Sol] = hb_nma(log10a_s,log10a_e, Nh, Nt,oscillator,...
+    om_lin, PHI_lin)
+
+
+inorm = 1;        % coordinate for phase normalization
+nmod = oscillator.n;
+
+% Initial guess vector y0 = [Psi;om;del], where del is the modal
+% damping ratio, estimate from underlying linear system
+om = om_lin(imod); phi = PHI_lin(:,imod);
+Psi = zeros((2*Nh+1)*nmod,1);
+Psi(Nmod+(1:Nmod)) = phi;
+x0 = [Psi;om;0];
+
+% set eps=1 to avoid step-refinement due to large residual. Prevent turning
+ds      = .1;
+Sopt    = struct('Dscale',[1e-6*ones(size(x0,1)-2,1);1;1e-1;1],...
+    'dynamicDscale',1,'stepmax',5e4,'eps',1);
+[X,Solinfo,Sol] = solve_and_continue(x0,...
+    @(X) HB_residual(X,oscillator,Nh,Nt,'nma',inorm),...
+    log10a_s,log10a_e,ds, Sopt);
+
+end
+
