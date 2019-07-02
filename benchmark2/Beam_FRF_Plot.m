f466cf79615f1acf730f85e944ac5897f8858679
eve lian <sl4912@ic.ac.uk>
2019-07-02 18:01:52 -0500

    Synthesized Phase + more damping on benchmark 2 mode 3

--- benchmark2/Beam_FRF_Plot.m
+++ benchmark2/Beam_FRF_Plot.m
@@ -39,7 +39,6 @@ plot(res{iex},peak_amp,'ok', 'LineWidth', 2)
 end
 xlabel('Forcing Frequency $\omega$ (Hz)')
 ylabel('RMS Response Displacement Amplitude (m)')
-% title('Flat Beam FRF -- Mode 1')
 xlim([100 650])
 
 plot(om_HB/2/pi,abs(a_w_L_2_NMA),'--k','linewidth',2)
@@ -57,6 +56,7 @@ figure
 semilogx(a_NMA,om_HB/2/pi,'linewidth',2);
 xlabel('Modal Amplitude'); ylabel('Natural Frequency (Hz)')
 
+% Illustrate Synthesized FRF
 figure
 p2 = (om_HB.^2-2*(del_HB.*om_HB).^2)';
 om4 = (om_HB.^4)';
@@ -91,8 +91,49 @@ legend('Fex = 10','Fex = 40','Fex = 60','Fex = 80','Fex = 100',...
     'Location','E');
 xlabel('Forcing Frequency $\omega$ (Hz)')
 ylabel('RMS Response Displacement Amplitude (m)')
-% title('Flat Beam FRF -- Mode 1')
 xlim([100 650])
+hold off
+
+% Illustrate Synthesized Phase Diagram
+figure; hold on;
+
+zts = del_HB;
+oms = om_HB;
+p2  = (oms.^2-2*(zts.*oms).^2)';
+om4 = (oms.^4)';
+Phi_HB = X_NM(n+(1:n),:)-1j*X_NM(2*n+(1:n),:);
+Fsc = (abs(Phi_HB'*Fex1)./a_NMA').^2;
+phf = Phi_HB'*Fex1;
+
+colos = distinguishable_colors(length(exc_lev));
+livs = 10;
+for k=1:length(exc_lev)
+    aa(k+1) = semilogy(Om{k}/(2*pi), phase{k}, ...
+        '-', 'LineWidth', 2, 'Color', colos(k,:)); hold on
+end
+
+for k=1:length(exc_lev)
+    om1 = sqrt(p2 + sqrt(p2.^2-om4+Fsc*exc_lev(k)^2));
+    om2 = sqrt(p2 - sqrt(p2.^2-om4+Fsc*exc_lev(k)^2));
+
+    ris1 = find(imag(om1)==0);  % real indices
+    ris2 = find(imag(om2)==0);  % real indices
+    
+    phi1 = rad2deg(angle(phf(ris1)./((oms(ris1)'.^2-om1(ris1).^2)+1j*(2*zts(ris1).*oms(ris1).*om1(ris1)')')));
+    phi2 = rad2deg(angle(phf(ris2)./((oms(ris2)'.^2-om2(ris2).^2)+1j*(2*zts(ris2).*oms(ris2).*om2(ris2)')')));
+    
+    plot(om1(ris1)/2/pi, phi1, '--', 'Color', colos(k,:))
+    plot(om2(ris2)/2/pi, phi2, '--', 'Color', colos(k,:))
+    
+    ivs = fix(linspace(1, length(ris1), livs));
+    plot(om1(ris1(ivs))/2/pi, phi1(ivs), '.', 'Color', colos(k,:), 'MarkerSize', 20)
+    ivs = fix(linspace(1, length(ris2), livs));
+    plot(om2(ris2(ivs))/2/pi, phi2(ivs), '.', 'Color', colos(k,:), 'MarkerSize', 20)
+end
+xlabel('Forcing Frequency $\omega$ (Hz)')
+ylabel('Response phase (degs)')
+legend('Fex = 10','Fex = 40','Fex = 60','Fex = 80','Fex = 100',...
+    'Location','E');
 hold off
 
 % figure
@@ -137,7 +178,6 @@ plot(res{iex},peak_amp,'ok', 'LineWidth', 2)
 end
 xlabel('Forcing Frequency $\omega$ (Hz)')
 ylabel('RMS Response Displacement Amplitude (m)')
-% title('Flat Beam FRF -- Mode 1')
 xlim ([1300 1600])
 ylim ([0 2.5e-4])
 
@@ -156,6 +196,7 @@ figure
 semilogx(a_NMA,om_HB/2/pi,'linewidth',2);
 xlabel('Modal Amplitude'); ylabel('Natural Frequency (Hz)')
 
+% Illustrate Synthesized FRF
 figure
 p2 = (om_HB.^2-2*(del_HB.*om_HB).^2)';
 om4 = (om_HB.^4)';
@@ -192,6 +233,49 @@ xlabel('Forcing Frequency $\omega$ (Hz)')
 ylabel('RMS Response Displacement Amplitude (m)')
 % title('Flat Beam FRF -- Mode 1')
 xlim([1300 1600])
+hold off
+
+% Illustrate Synthesized Phase Diagram
+figure; hold on;
+
+zts = del_HB;
+oms = om_HB;
+p2  = (oms.^2-2*(zts.*oms).^2)';
+om4 = (oms.^4)';
+Phi_HB = X_NM(n+(1:n),:)-1j*X_NM(2*n+(1:n),:);
+Fsc = (abs(Phi_HB'*Fex1)./a_NMA').^2;
+phf = Phi_HB'*Fex1;
+
+colos = distinguishable_colors(length(exc_lev));
+livs = 10;
+for k=1:length(exc_lev)
+    aa(k+1) = semilogy(Om{k}/(2*pi), phase{k}, ...
+        '-', 'LineWidth', 2, 'Color', colos(k,:)); hold on
+end
+
+for k=1:length(exc_lev)
+    om1 = sqrt(p2 + sqrt(p2.^2-om4+Fsc*exc_lev(k)^2));
+    om2 = sqrt(p2 - sqrt(p2.^2-om4+Fsc*exc_lev(k)^2));
+
+    ris1 = find(imag(om1)==0);  % real indices
+    ris2 = find(imag(om2)==0);  % real indices
+    
+    phi1 = rad2deg(angle(phf(ris1)./((oms(ris1)'.^2-om1(ris1).^2)+1j*(2*zts(ris1).*oms(ris1).*om1(ris1)')')))+180;
+    phi2 = rad2deg(angle(phf(ris2)./((oms(ris2)'.^2-om2(ris2).^2)+1j*(2*zts(ris2).*oms(ris2).*om2(ris2)')')))+180;
+    
+    plot(om1(ris1)/2/pi, phi1, '--', 'Color', colos(k,:))
+    plot(om2(ris2)/2/pi, phi2, '--', 'Color', colos(k,:))
+    
+    ivs = fix(linspace(1, length(ris1), livs));
+    plot(om1(ris1(ivs))/2/pi, phi1(ivs), '.', 'Color', colos(k,:), 'MarkerSize', 20)
+    ivs = fix(linspace(1, length(ris2), livs));
+    plot(om2(ris2(ivs))/2/pi, phi2(ivs), '.', 'Color', colos(k,:), 'MarkerSize', 20)
+end
+xlabel('Forcing Frequency $\omega$ (Hz)')
+ylabel('Response phase (degs)')
+xlim ([1300 1600])
+legend('Fex = 10','Fex = 40','Fex = 60','Fex = 80','Fex = 100',...
+    'Location','E');
 hold off
 
 % figure
@@ -241,7 +325,6 @@ plot(res{iex},peak_amp,'ok', 'LineWidth', 2)
 end
 xlabel('Forcing Frequency $\omega$ (Hz)')
 ylabel('RMS Response Displacement Amplitude (m)')
-% title('Flat Beam FRF -- Mode 1')
 xlim ([3480 3600])
 ylim ([0 3.1e-5])
 
@@ -250,6 +333,7 @@ legend('Fex = 10','Fex = 40','Fex = 60','Fex = 80','Fex = 100','EPMC',...
     'Location','E');
 hold off
 
+% Modal Amplitude vs Damping Factor
 figure
 Y_HB_1 = Q_HB(n+(1:n),:)-1i*Q_HB(2*n+(1:n),:);
 semilogx(a_NMA,del_HB*1e2,'b-','LineWidth',2);
@@ -257,10 +341,12 @@ semilogx(a_NMA,del_HB*1e2,'b-','LineWidth',2);
 xlabel('Modal Amplitude'); ylabel('Damping Factor')
 title('Damping Ratio')
 
+% Modal Amplitude vs Natural Frequency
 figure
 semilogx(a_NMA,om_HB/2/pi,'linewidth',2);
 xlabel('Modal Amplitude'); ylabel('Natural Frequency (Hz)')
 
+% Forcing Frequency vs RMS Response Displacement Amplitude
 figure
 p2 = (om_HB.^2-2*(del_HB.*om_HB).^2)';
 om4 = (om_HB.^4)';
@@ -295,6 +381,48 @@ legend('Fex = 10','Fex = 40','Fex = 60','Fex = 80','Fex = 100',...
     'Location','E');
 xlabel('Forcing Frequency $\omega$ (Hz)')
 ylabel('RMS Response Displacement Amplitude (m)')
-% title('Flat Beam FRF -- Mode 1')
 xlim ([3480 3600])
+hold off
+
+%% Illustrate Synthesized Phase Diagram
+figure; hold on;
+
+zts = del_HB;
+oms = om_HB;
+p2  = (oms.^2-2*(zts.*oms).^2)';
+om4 = (oms.^4)';
+Phi_HB = X_NM(n+(1:n),:)-1j*X_NM(2*n+(1:n),:);
+Fsc = (abs(Phi_HB'*Fex1)./a_NMA').^2;
+phf = Phi_HB'*Fex1;
+
+colos = distinguishable_colors(length(exc_lev));
+livs = 10;
+for k=1:length(exc_lev)
+    aa(k+1) = semilogy(Om{k}/(2*pi), phase{k}, ...
+        '-', 'LineWidth', 2, 'Color', colos(k,:)); hold on
+end
+
+for k=1:length(exc_lev)
+    om1 = sqrt(p2 + sqrt(p2.^2-om4+Fsc*exc_lev(k)^2));
+    om2 = sqrt(p2 - sqrt(p2.^2-om4+Fsc*exc_lev(k)^2));
+
+    ris1 = find(imag(om1)==0);  % real indices
+    ris2 = find(imag(om2)==0);  % real indices
+    
+    phi1 = rad2deg(angle(phf(ris1)./((oms(ris1)'.^2-om1(ris1).^2)+1j*(2*zts(ris1).*oms(ris1).*om1(ris1)')')));
+    phi2 = rad2deg(angle(phf(ris2)./((oms(ris2)'.^2-om2(ris2).^2)+1j*(2*zts(ris2).*oms(ris2).*om2(ris2)')')));
+    
+    plot(om1(ris1)/2/pi, phi1, '--', 'Color', colos(k,:))
+    plot(om2(ris2)/2/pi, phi2, '--', 'Color', colos(k,:))
+    
+    ivs = fix(linspace(1, length(ris1), livs));
+    plot(om1(ris1(ivs))/2/pi, phi1(ivs), '.', 'Color', colos(k,:), 'MarkerSize', 20)
+    ivs = fix(linspace(1, length(ris2), livs));
+    plot(om2(ris2(ivs))/2/pi, phi2(ivs), '.', 'Color', colos(k,:), 'MarkerSize', 20)
+end
+xlabel('Forcing Frequency $\omega$ (Hz)')
+ylabel('Response phase (degs)')
+xlim ([3480 3600])
+legend('Fex = 10','Fex = 40','Fex = 60','Fex = 80','Fex = 100',...
+    'Location','E');
 hold off
